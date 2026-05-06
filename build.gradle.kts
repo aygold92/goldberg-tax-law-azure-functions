4e161205542619e236d16c1b25c4e7e4ba1ab50b
Andrew Goldberg <agoldberg928@yahoo.com>
2026-05-06 14:15:14 +0200

    fix logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,9 +63,7 @@ dependencies {
     // guice
     implementation("com.google.inject:guice:7.0.0")
     // logging
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.23.1") // Log4j SLF4J binding
-    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
-    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("org.slf4j:slf4j-jdk14:2.0.16")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0") // Kotlin-friendly logging wrapper
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/goldberg/law/function/FunctionGuiceFactory.kt
+++ src/main/kotlin/com/goldberg/law/function/FunctionGuiceFactory.kt
@@ -3,6 +3,11 @@ package com.goldberg.law.function
 import com.goldberg.law.AppModule
 import com.google.inject.Guice
 import com.microsoft.azure.functions.spi.inject.FunctionInstanceInjector
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
+import java.util.logging.LogManager
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
 
 class FunctionGuiceFactory : FunctionInstanceInjector {
 
@@ -12,6 +17,27 @@ class FunctionGuiceFactory : FunctionInstanceInjector {
     }
 
     companion object {
+        init {
+            configureLogging()
+        }
+
         private val INJECTOR = Guice.createInjector(AppModule())
+
+        private fun configureLogging() {
+            System.setProperty(
+                "java.util.logging.SimpleFormatter.format",
+                "[%4\$s] %3\$s - %5\$s%6\$s%n"
+            )
+            val root = LogManager.getLogManager().getLogger("") ?: return
+            root.handlers.filterIsInstance<ConsoleHandler>().forEach { root.removeHandler(it) }
+            root.addHandler(ConsoleHandler().apply {
+                formatter = SimpleFormatter()
+                level = Level.ALL
+            })
+            Logger.getLogger("com.goldberg.law").level = Level.FINE  // DEBUG and above
+            Logger.getLogger("com.zaxxer.hikari").level = Level.WARNING
+            Logger.getLogger("com.azure").level = Level.WARNING
+            Logger.getLogger("com.microsoft").level = Level.WARNING
+        }
     }
 }
